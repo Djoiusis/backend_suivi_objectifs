536f506875d8005d2068a8b48e7bf46e644a09c4
Djoiusis <jihaadbenyedder@gmail.com>
2025-10-14 17:57:52 +0200

    Create requireBUM.js

---
+++ middlewares/requireBUM.js
@@ -0,0 +1,9 @@
+// middleware/requireBUM.js
+function requireBUM(req, res, next) {
+  if (req.user.role !== 'BUM') {
+    return res.status(403).json({ error: 'Accès réservé aux BUM' });
+  }
+  next();
+}
+
+module.exports = requireBUM;
