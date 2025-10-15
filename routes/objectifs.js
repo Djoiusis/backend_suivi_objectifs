cfbd88e661ad24a6fe27a75821308839a9921a0c
Djoiusis <jihaadbenyedder@gmail.com>
2025-10-15 14:32:40 +0200

    Update objectifs.js

--- routes/objectifs.js
+++ routes/objectifs.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
-const { verifyToken, requireAdmin, requireAdminOrBUM } = require('../middleware/auth');
-
+const { verifyToken, requireAdmin, requireAdminOrBUM } = require('./auth');
 const prisma = new PrismaClient();
 
 // Récupérer les objectifs du consultant connecté
